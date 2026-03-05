27c73e633b657fbcd8ceef0db646e11032654836
adewaley-a <abdullahiadewale3@gmail.com>
2026-03-05 16:16:13 +0100

    b

--- server.js
+++ server.js
@@ -13,29 +13,22 @@ const rooms = {};
 io.on('connection', (socket) => {
   socket.on('join_game', ({ roomId }) => {
     socket.join(roomId);
-    
     if (!rooms[roomId]) {
       rooms[roomId] = { players: {}, health: { host: 400, guest: 400 } };
     }
-
-    // Role Assignment Logic
     const clients = io.sockets.adapter.rooms.get(roomId);
-    const numPlayers = clients ? clients.size : 0;
-    
-    // First person is host, everyone else is guest
-    const role = numPlayers === 1 ? 'host' : 'guest';
-    
-    rooms[roomId].players[socket.id] = role;
+    const role = (clients && clients.size === 1) ? 'host' : 'guest';
     socket.emit('assign_role', { role });
-    
     io.in(roomId).emit('update_health', rooms[roomId].health);
   });
 
   socket.on('move', (data) => {
+    // data includes { x, y, angle }
     socket.to(data.roomId).emit('opp_move', data);
   });
 
   socket.on('fire', (data) => {
+    // data includes { x, y, angle, vx, vy }
     socket.to(data.roomId).emit('incoming_bullet', data);
   });
 
