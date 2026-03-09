e1755bcb6e74a5c80c1f2e39211917df5511fb20
adewaley-a <abdullahiadewale3@gmail.com>
2026-03-09 12:19:47 +0100

    u

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server, { 
-  cors: { origin: "https://deatwin.netlify.app" } 
-});
+const io = new Server(server, { cors: { origin: "https://deatwin.netlify.app" } });
 
 const rooms = {};
 
@@ -18,18 +16,16 @@ io.on('connection', (socket) => {
     if (!rooms[roomId]) {
       rooms[roomId] = { host: socket.id, guest: null, health: { host: 400, guest: 400 } };
       socket.emit('assign_role', { role: 'host' });
-    } else if (!rooms[roomId].guest && rooms[roomId].host !== socket.id) {
+    } else if (!rooms[roomId].guest && socket.id !== rooms[roomId].host) {
       rooms[roomId].guest = socket.id;
       socket.emit('assign_role', { role: 'guest' });
-    } else {
-      if (socket.id === rooms[roomId].host) socket.emit('assign_role', { role: 'host' });
-      else if (socket.id === rooms[roomId].guest) socket.emit('assign_role', { role: 'guest' });
     }
     io.in(roomId).emit('update_health', rooms[roomId].health);
   });
 
   socket.on('move', (data) => socket.to(data.roomId).emit('opp_move', data));
   socket.on('fire', (data) => socket.to(data.roomId).emit('incoming_bullet', data));
+  
   socket.on('take_damage', ({ roomId, victimRole }) => {
     if (rooms[roomId]) {
       rooms[roomId].health[victimRole] = Math.max(0, rooms[roomId].health[victimRole] - 2);
